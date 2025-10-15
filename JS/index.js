705c638e882d4e042ad260a2e2e280fc58c5e36f
Pixaiti <20251AC0080013@estudantes.ifpr.edu.br>
2025-10-15 10:55:22 -0300

    ai ai ai

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const elements = document.querySelectorAll('.reveal');
+
+elements.forEach(el => observer.observe(el));
